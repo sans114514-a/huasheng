76904e8f101f7c5157fc2178181edc6d416089ee
sans114514-a <117575989+sans114514-a@users.noreply.github.com>
2022-11-11 20:51:43 +0800

    MakeCode 项目的初始文件

---
+++ main.py
@@ -0,0 +1,11 @@
+basic.show_string("RBT")
+basic.pause(1000)
+basic.show_leds("""
+    .# # . #
+        . # # # .
+        # # # # .
+        . # # . #
+        . . # . .
+""")
+basic.pause(1000)
+basic.show_string("thanks！")
---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.showString("RBT")
+basic.pause(1000)
+basic.showLeds(`
+    . . # . #
+    . # # # .
+    # # # # .
+    . # # . #
+    . . # . .
+    `)
+basic.pause(1000)
+basic.showString("thanks！")
